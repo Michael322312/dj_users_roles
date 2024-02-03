ee19308cff28722236eee5639cc28e407a520d18
Michael322312 <zomisha0808@gmail.com>
2024-02-03 12:36:32 +0200

    Recreated django apps and project

--- users/models.py
+++ users/models.py
@@ -1,11 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    role = models.ManyToManyField("Role", related_name="roles")
-
-class Role(models.Model):
-    name = models.CharField(max_length=100)
